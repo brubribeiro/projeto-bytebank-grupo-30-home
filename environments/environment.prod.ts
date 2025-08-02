11b1254e6b2174e45fc038b447c11ffa6cf15773
Herbert Rezende Ferreira <39607526+hbtferreira@users.noreply.github.com>
2025-08-02 15:14:59 -0300

    Feature/login service integration form validation (#2)
    * Substitui ícone
    
    * Ajusta a model para ficar igual ao user no backend
    
    * Ajusta as propriedades para ficar igual ao backend
    
    * Implementa métodos e tratamento de erro
    
    * Validação dos formulários e integração do backend
    
    * Autenticação centralizada

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -4,4 +4,8 @@ export const environment = {
   apiBaseUrl: 'http://localhost:3000',
   apiVersion: '/v1',
   apiNamespace: '/bytebank',
+  shell: {
+    remoteEntry: 'https://shell.bytebank.com/remoteEntry.js',
+    remoteName: 'shell-projeto-bytebank-gp-30'
+  }
 };
